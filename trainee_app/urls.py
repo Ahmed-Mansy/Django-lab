2685260f301c1889c687362989a72a09c142bec5
Ahmed-Mansy <Ahmedalimansy1@gmail.com>
2025-03-20 12:09:44 +0200

    Django Labs

---
+++ trainee_app/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import TraineeListCreateAPIView, TraineeUpdateDeleteAPIView, track_update, TraineeViewSet
+
+router = DefaultRouter()
+# from .views import TraineeListView, TraineeListCreateAPIView, TraineeUpdateDeleteAPIView, track_update, TraineeViewSet
+from .views import TraineeListView, TraineeDeleteView, AddTraineeView, UpdateTraineeView
+
+router.register(r'trainees', TraineeViewSet)
+
+
+urlpatterns = [
+    path('trainees/', TraineeListCreateAPIView.as_view(), name='trainee-list-create'),
+    path('trainees/<int:pk>/', TraineeUpdateDeleteAPIView.as_view(), name='trainee-update-delete'),
+    path('track_update/<int:pk>/', track_update, name='track-update'),
+    path('', include(router.urls)),
+]
+
